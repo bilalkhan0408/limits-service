b3f806659d844c173e6d509b9674634d7cd5fde3
bilalkhan0408 <bilalkhan0408@gmail.com>
2021-08-16 19:28:31 +0530

    commiting all the code of limit service

---
+++ src/main/java/com/in28minutes/microservices/limitsservice/LimitsConfigurationController.java
@@ -0,0 +1,24 @@
+package com.in28minutes.microservices.limitsservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.in28minutes.microservices.limitsservice.beans.LimitConfiguration;
+
+@RestController
+public class LimitsConfigurationController {
+
+
+	@Autowired
+	private Configuration con;
+	
+	
+@GetMapping("/limits")
+public LimitConfiguration retrieveLimitFromConfiguation () {
+	
+	return new LimitConfiguration(con.getMaximum(),con.getMinimum());
+	
+	
+}	
+}
