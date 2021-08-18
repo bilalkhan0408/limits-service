0e56f4cb2dde55baacca3a114995a411f19be263
Mo Bilal Khan <A1Q7EQIA@india.airtel.itm>
2021-08-18 14:47:10 +0530

    controller change

--- src/main/java/com/in28minutes/microservices/limitsservice/LimitsConfigurationController.java
+++ src/main/java/com/in28minutes/microservices/limitsservice/LimitsConfigurationController.java
@@ -17,6 +17,8 @@ public class LimitsConfigurationController {
 @GetMapping("/limits")
 public LimitConfiguration retrieveLimitFromConfiguation () {
 	
+	
+	new LimitConfiguration();
 	return new LimitConfiguration(con.getMaximum(),con.getMinimum());
 	
 	
