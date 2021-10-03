01cfae0e39e5896991889cb4919cd1d968ca7df6
bilalkhan0408 <79372743+bilalkhan0408@users.noreply.github.com>
2021-10-03 15:58:57 +0530

    Update LimitsServiceApplication.java

--- src/main/java/com/in28minutes/microservices/limitsservice/LimitsServiceApplication.java
+++ src/main/java/com/in28minutes/microservices/limitsservice/LimitsServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LimitsServiceApplication {
-
+// hiiiiiiiiiiiis
 	public static void main(String[] args) {
 		SpringApplication.run(LimitsServiceApplication.class, args);
 	}
